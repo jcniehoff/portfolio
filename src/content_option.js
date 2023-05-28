9229673dc06c1ac97d54c56f4ae777b2b965d5e0
jcniehoff <91128577+jcniehoff@users.noreply.github.com>
2023-05-28 20:15:19 +0200

    Add files via upload
    initial commit

---
+++ src/content_option.js
@@ -0,0 +1,177 @@
+const logotext = "Jan Niehoff";
+const meta = {
+    title: "Jan Niehoff",
+    description: "I’m Jan Niehoff, student in mathematics _ studying in Mannheim ",
+};
+
+const introdata = {
+    title: "I’m Jan",
+    animated: {
+        first: "I learn coding",
+        second: "I learn Data Analysis",
+        third: "I learn Data Science",
+        fourth: "I learn Data Visualization",
+        fifth: "I learn Data.",
+    },
+    description: "Math Student with a Statistics Background and real experience in Python, R and SQL.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme: "Experienced data scientist with a passion for leveraging data-driven insights to solve complex business problems. Skilled in statistical analysis, machine learning, and data visualization, with a proven track record of delivering impactful solutions for clients across various industries. Proficient in programming languages such as Python and R, with experience in data manipulation and cleaning. Strong communicator and collaborator, with a focus on building positive relationships with stakeholders. Committed to continuous learning and staying up-to-date with the latest trends and technologies in the field.",
+};
+const worktimeline = [{
+        jobtitle: "President at Students' Association for Data Analytics and Statistics Mannheim e.V.",
+        where: "Manheim",
+        date: "2022-2023",
+    },
+    {
+        jobtitle: "Intern in Risk Advisory and the aiStudio at Deloitte",
+        where: "Frankfurt",
+        date: "2022",
+    },
+    //{
+    //    jobtitle: "Designer of week",
+    //    where: "ALquds",
+    //    date: "2019",
+    //},
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "SQL",
+        value: 85,
+    },
+    {
+        name: "R",
+        value: 80,
+    },
+    //{
+    //    name: "React",
+    //    value: 60,
+    //},
+    //{
+    //    name: "Jquery",
+    //    value: 85,
+    //},
+];
+
+const interests = [{
+        title: "Data Analysis",
+        description: "I enjoy analyzing because it helps me find important insights that can improve my skills. By exploring the data, I can identify patterns and trends that might not be immediately apparent, and use that information to create meaningful projects that showcase my abilities.",
+    },
+    {
+        title: "Data Visualization",
+        description: "I find data visualization to be a powerful tool for presenting data in a clear and compelling way. I like creating visualizations that help me understand the data better and that can highlight key insights and trends. Whether it's a simple chart or a more complex dashboard, I find that data visualization can be a great way to tell a story with data.",
+    },
+    {
+        title: "Data Science",
+        description: " I'm passionate about data science because it combines my mathematical background with coding and domain expertise to solve real-world problems. I enjoy working on data science projects to learn and build my portfolio, where I can experiment with different techniques and tools to extract insights and make predictions. I also appreciate the collaborative nature of data science, which allows me to work with others and data from different fields.",
+    },
+    {
+        title: "Machine Learning",
+        description: "I'm excited about the possibilities of using machine learning to build my skills and create impactful projects for my portfolio. I enjoy trying different algorithms and techniques, and finding creative ways to apply them to real-world problems. Machine learning has the potential to automate tasks and make better decisions, and I'm eager to explore its potential.",
+    },
+    /* {
+        title: "Machine Learning",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    }, */
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials. - Albert Einstein",
+        link: "/home",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials. - Isaac Newton",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    /* {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    }, */
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "jan.niehoff@stads.de",
+    YOUR_FONE: "+49 162 8845213",
+    description: "Please get in touch with me using the above email or the LinkedIn below, the contact form isnt working yet.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/jcniehoff",
+    // facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/jan-niehoff",
+    // twitter: "https://twitter.com",
+    email: "jan.niehoff@stads.de",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    interests,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
